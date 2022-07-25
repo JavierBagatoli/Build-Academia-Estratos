c335c40ead8168befbd93eafaf6f054fcf3cc4b3
Joan Gabriel Salas Silva <joan.salas.silva@gmail.com>
2022-07-25 17:05:29 -0400

    feat: crearDocumentacion list

--- src/controllers/documentacion.controller.js
+++ src/controllers/documentacion.controller.js
@@ -4,12 +4,26 @@ import { getPagination } from "../libs/getPagination";
 // Creado por Joan Salas 25/07
 
 export const obtenerDocumentaciones = async (req, res) => {
-  try {
-    const { size, page } = req.query;
-    const { limit, offset } = getPagination(page, size);
-    const documentaciones = await Documentacion.paginate({}, { offset, limit });
-    res.status(200).json(documentaciones);
-  } catch (e) {
-    mensajeError(res, "Fallo al obtenerse las documentaciones");
-  }
+    try {
+        const { size, page } = req.query;
+        const { limit, offset } = getPagination(page, size);
+        const documentaciones = await Documentacion.paginate({}, { offset, limit });
+        res.status(200).json(documentaciones);
+    } catch (e) {
+        mensajeError(res, "Fallo al obtenerse las documentaciones");
+    }
 };
+
+// TODO JS: Crear y actualizar
+
+export const crearDocumentacion = async (req, res) => {
+    try {
+        const datos = req.body
+        //TODO JS: Validar datos
+        const nuevaDocumentacion = new Documentacion(datos)
+        const documentacionCreada = await nuevaDocumentacion.save()
+        res.status(201).json(documentacionCreada)
+    } catch (e) {
+        res.json(e)
+    }
+}
