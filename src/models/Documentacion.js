843929d58d3fcecc99de1e8f9bc5b8f596df7e02
JavierBagatoli <javi.bagatoli@gmail.com>
2022-07-25 17:35:07 -0300

    feat: modelo de documetnacion

---
+++ src/models/Documentacion.js
@@ -0,0 +1,19 @@
+import { Schema, model } from "mongoose";
+import mongoosePaginate from "mongoose-paginate-v2";
+
+const documentacionEsquema = new Schema(
+  {
+    titulo: { type: String, required: true, trim: true },
+    especialidad: { type: String, required: true, trim: true },
+    tema: { type: String, required: true, trim: true },
+    descripcion: { type: String, required: true, trim: true },
+    enlaces: { type: Array, required: true, trim: true },
+    autor: { type: String, required: true, trim: true },
+  },
+  {
+    versionKey: false,
+    timestamps: true,
+  }
+);
+documentacionEsquema.plugin(mongoosePaginate);
+export default model("Documentacion", documentacionEsquema);
